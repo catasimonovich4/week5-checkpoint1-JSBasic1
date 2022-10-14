13ec05c464b109fa6d65e6dce301cf3b9f76d16d
Catalina Simonovich <catalinasimonovich1@gmail.com>
2022-10-14 15:37:11 -0300

    :tada:Feat: Create new repository with work from week5, Chekpoint1 introducing JS Basics.

---
+++ script.js
@@ -0,0 +1,47 @@
+//Create variables
+
+//Type: String
+const name = 'Catalina'; 
+console.log(`User's name: ${name}`);
+//Type: String
+const lastName = 'Simonovich';
+console.log(`User's last name: ${lastName}`);
+//Type: Number
+const bday = 11299; //DMMAA
+console.log(`User's birthday (D-MM-AA): ${bday}`);
+//Type: Number
+const age = 22;
+console.log(`User's age: ${age}`);
+//Type: Number
+const id = 42399766 //DNI
+console.log(`User's ID number: ${id}`);
+//Type: String
+var city = "Bahia Blanca";
+console.log(`User's current city: ${city}`);
+//Type: String
+var profession = "Software Engineering student";
+console.log(`User's profession: ${profession}`);
+//Type: Boolean
+var single = false;
+console.log(`Is user single?: ${single}`);
+//Type: String
+var footballTeam = "Boca Juniors";
+console.log(`User's football team: ${footballTeam}`);
+//Type: Boolean
+var pets = false;
+console.log(`Does user have pets?: ${pets}`);
+//Type: Array
+var favArtist = ["Noel Gallagher", "RHCP" ,"Alex Turner", "Dua Lipa", "Nicki Nicole"];
+console.log(`User's favourites artist: ${favArtist}`);
+//Type: Object
+var sport = {
+    type: "Volleyball",
+    position: ["Punta-receptora", "Libero"],
+    competition: true,
+    club: "Universidad Nacional del Sur",
+    shirtNumber: 4
+}
+console.log("User's sport:", sport);  
+
+
+
